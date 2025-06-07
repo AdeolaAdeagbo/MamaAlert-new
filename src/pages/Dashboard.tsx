cd27366b9baf425d452e63f9722b6de3dbd8a573
AdeolaAdeagbo <adeagbozain50@gmail.com>
2025-06-08 00:16:04 +0100

    badge errpr

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -6,6 +6,7 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useToast } from "@/hooks/use-toast";
 import { Navigate, Link } from "react-router-dom";
+import { Badge } from "@/components/ui/badge";
 import { 
   AlertTriangle, 
   Heart, 
